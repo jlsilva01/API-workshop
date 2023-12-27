b84f641e74c1e5d8afd30f444d219f7049adc044
Jorge Luiz da Silva <jlsilva01@yahoo.com.br>
2023-12-26 23:04:02 -0300

    formatando o codigo com o pydocstyle

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-""" Arquivo principal do programa."""
+"""File that contains the application code."""
+
 from fastapi import FastAPI
 from .schema import ProdutosSchema
 from .data import Produtos
@@ -9,20 +10,20 @@ lista_de_produtos = Produtos()
 
 @app.get("/")
 async def ola_mundo():
-    """ Função que retorna uma mensagem de olá mundo."""
+    """Função que retorna uma mensagem de olá mundo."""
     return {"Ola": "Mundo"}
 
 @app.get("/produtos", response_model=list[ProdutosSchema])
 async def listar_produtos():
-    """ Função que retorna a lista de produtos."""
+    """Função que retorna a lista de produtos."""
     return lista_de_produtos.listar_produtos()
 
 @app.get("/produto/{id}", response_model=ProdutosSchema)
 async def buscar_produto(id: int):
-    """ Função que retorna um produto."""
+    """Função que retorna um produto."""
     return lista_de_produtos.buscar_produto(id)
 
 @app.post("/produto", response_model=ProdutosSchema)
 async def adicionar_produto(produto: ProdutosSchema):
-    """ Função que cria um produto."""
+    """Função que cria um produto."""
     return lista_de_produtos.adicionar_produto(produto)
